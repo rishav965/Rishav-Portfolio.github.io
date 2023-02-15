427e7c452f1c161a22573c679784ca17719aaad3
Rishav Kumar <96387480+rishav965@users.noreply.github.com>
2023-02-15 12:55:33 +0530

    My Portfolio

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+    strings: ["Tech Enthusiast", "Tech Enthusiast", "Front-End Developer", "Python Coder"],
+    typeSpeed: 100,
+    backSpeed: 100, 
+    backdelay: 1000, 
+    loop: true
+})
